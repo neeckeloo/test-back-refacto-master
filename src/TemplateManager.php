2fa8aa31e9404c1c6c1253266ae875b86b6caf56
Nicolas Eeckeloo <neeckeloo@gmail.com>
2020-06-30 10:02:33 +0200

    Remove useless thrown exception

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -11,10 +11,6 @@ class TemplateManager
 
     public function getTemplateComputed(Template $tpl, array $data)
     {
-        if (!$tpl) {
-            throw new \RuntimeException('no tpl given');
-        }
-
         $replaced = clone($tpl);
         $replaced->subject = $this->computeText($replaced->subject, $data);
         $replaced->content = $this->computeText($replaced->content, $data);
